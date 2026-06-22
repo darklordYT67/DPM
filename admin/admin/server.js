7e69199e16d5b7549669edc2ccb01bd7b896f812
darklordYT67 <remained67@proton.me>
2026-06-22 17:25:32 -0500

    Set up basic Express server on port 3000

--- admin/admin/server.js
+++ admin/admin/server.js
@@ -1,1 +1,11 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("DPM Server Running");
+});
+
+app.listen(3000, () => {
+    console.log("DPM server started on port 3000");
+});
